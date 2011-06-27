82b332cb786497aeb9da0006380ecca26109095a
Matthew Horsfall <WolfSage@cpan.org>
2011-06-26 21:43:17 -0400

    Update pod a little in provider classes

--- lib/Devel/MemoryTrace/Light/BSDProcess.pm
+++ lib/Devel/MemoryTrace/Light/BSDProcess.pm
@@ -25,10 +25,25 @@ sub forked {
 
 =head1 NAME
 
-Devel::MemoryTrace::Light::BSDProcess - L<BSD::Process>
+Devel::MemoryTrace::Light::BSDProcess - A L<BSD::Process> memory provider
+
+=head1 SYNOPSIS
+
+Do not use this module directly. See L<Devel::MemoryTrace::Light>
 
 =head1 DESCRIPTION
 
 Provides a L<BSD::Process> memory examiner to L<Devel::MemoryTrace::Light>
+
+=head1 AUTHOR
+
+Matthew Horsfall (alh) - <WolfSage@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Matthew Horsfall.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
--- lib/Devel/MemoryTrace/Light/GTop.pm
+++ lib/Devel/MemoryTrace/Light/GTop.pm
@@ -23,10 +23,25 @@ sub forked {
 
 =head1 NAME
 
-Devel::MemoryTrace::Light::GTop - L<GTop>
+Devel::MemoryTrace::Light::GTop - A L<GTop> memory provider
+
+=head1 SYNOPSIS
+
+Do not use this module directly. See L<Devel::MemoryTrace::Light>
 
 =head1 DESCRIPTION
 
 Provides a L<GTop> memory examiner to L<Devel::MemoryTrace::Light>
+
+=head1 AUTHOR
+
+Matthew Horsfall (alh) - <WolfSage@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Matthew Horsfall.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
